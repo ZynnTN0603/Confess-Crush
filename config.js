060c56d5df82b3e387b1ce0b0653858a29390451
ZynnTN0603 <94165820+ZynnTN0603@users.noreply.github.com>
2021-11-12 15:32:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi con mắm nào đó",
+    introTitle: 'Đoán xem là ai :>, nhột thì tự chịu, tui không biết gì đou.',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Đang dịch mà, không đi chơi đc đou ;-; đi là hai đứa đi cách li lun á',
+    btnAccept: 'Thôi không sao hết nè <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
